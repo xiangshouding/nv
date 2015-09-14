e215a3701d8cc3bfef0605de5f29d9694bab66c7
xiangshouding <fansekey@gmail.com>
2015-09-14 10:22:14 +0800

    support marked(by node) processor

---
+++ NSString_Marked.h
@@ -0,0 +1,13 @@
+//
+//  NSString_Marked.h
+//  
+//
+//  Created by shouding on 15/9/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString_Marked : NSString
+
+@end
---
+++ NSString_Marked.m
@@ -0,0 +1,13 @@
+//
+//  NSString_Marked.m
+//  
+//
+//  Created by shouding on 15/9/14.
+//
+//
+
+#import "NSString_Marked.h"
+
+@implementation NSString_Marked
+
+@end
